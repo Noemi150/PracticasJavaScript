4ea6faec6b91b5288d581bc0dc0193ae535f5d2b
Noemi150 <dorinoemi252@gmail.com>
2020-11-03 09:59:58 -0600

    Calificaciones

--- Tareas/JS/calificación.js
+++ Tareas/JS/calificación.js
@@ -15,15 +15,11 @@ function calcularcalificacion(){
     if(calificacion == 8){
         imprimircal.innerText = "Tu calificación es  C"
     }
-    if(calificacion == 7 && calificacion == 6){
+    if(calificacion == 7 || calificacion == 6){
         imprimircal.innerText = "Tu calificación es  D"
     }
-    // if(calificacion == 5 && calificacion == 4 && calificacion == 3 && calificacion == 2 && calificacion == 1 && calificacion == 0){
-    //     imprimircal.innerText = "Tu calificación es F"
-    // }
-    if(calificacion <=5){
-        imprimircal.innerText = "Tu calificación es F"
-    }
-
-    
+    if(calificacion == 5 || calificacion == 4 || calificacion == 3 || calificacion == 2 || calificacion == 1 || calificacion == 0){
+       imprimircal.innerText = "Tu calificación es F"
+     }
+     
 }
